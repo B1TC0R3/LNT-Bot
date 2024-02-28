2605fb6ce44e13737128a9f7ccbb2d5a81d57f4a
B1TC0R3 <b1tc0r3@proton.me>
2024-02-28 22:12:02 +0100

    Adapt main.py to nerw config format

--- src/main.py
+++ src/main.py
@@ -1,15 +1,11 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from config import TOKEN, GUILD
 
-guild_id = discord.Object(id=769289266898403328)
-
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-token = ""
-with open("token", "r") as token_file:
-    token = token_file.read()
+guild_id = discord.Object(id=GUILD)
+intents  = discord.Intents.all()
+bot      = commands.Bot(command_prefix='!', intents=intents)
 
 
 @bot.event
@@ -23,7 +19,9 @@ async def on_ready():
 
 
 @bot.tree.command(name="ping", guild=guild_id)
-async def ping(interaction: discord.Interaction):
+async def ping(
+    interaction: discord.Interaction
+):
     await interaction.response.send_message("Pong. I am alive.")
  
 
@@ -33,12 +31,16 @@ async def ping(interaction: discord.Interaction):
     description="Description of the resource",
     url="The link to the resource"
 )
-async def resource(interaction: discord.Interaction, title: str, url: str, description: str=''):
+async def resource(
+    interaction: discord.Interaction,
+    title: str,
+    url: str,
+    description: str=''
+):
     embed = discord.Embed(title=title, description=description)
     embed.add_field(name="URL", value=url)
 
     await interaction.response.send_message(embed=embed)
-    pass
 
 
-bot.run(token)
+bot.run(TOKEN)
